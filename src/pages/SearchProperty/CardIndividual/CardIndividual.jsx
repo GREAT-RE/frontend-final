9d09183807347eb197463a84b42acbb4c6456f4e
JoseSNunes <josesimasnunes@gmail.com>
2023-05-17 10:55:18 +0100

    Fixed card individual page

--- src/pages/SearchProperty/CardIndividual/CardIndividual.jsx
+++ src/pages/SearchProperty/CardIndividual/CardIndividual.jsx
@@ -7,26 +7,118 @@ import ListingDetailsMap from "../../../components/MapListingDetails/ListingDeta
 import MapListing from "../../../components/MapListingDetails/MapListing";
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import imagePlaceholder from "../../../assets/placeholder-image.png";
+import airConditioner from "../../../assets/amenity-icons/air-conditioner.svg";
+import wifi from "../../../assets/amenity-icons/wifi.svg";
+import cableTv from "../../../assets/amenity-icons/cable-tv.svg";
+import heating from "../../../assets/amenity-icons/heating.svg";
+import kitchen from "../../../assets/amenity-icons/kitchen.svg";
+import microwave from "../../../assets/amenity-icons/microwave.svg";
+import oven from "../../../assets/amenity-icons/oven.svg";
+import refrigerator from "../../../assets/amenity-icons/refrigerator.svg";
+import stove from "../../../assets/amenity-icons/stove.svg";
+import washer from "../../../assets/amenity-icons/washer.svg";
+import axios from "axios";
+
 const CardIndividual = () => {
   const [singleProperty, setSingleProperty] = useState();
   const [src, setSrc] = useState();
+  const [amenities, setAmenities] = useState();
+  const [address, setAddress] = useState();
+  const [amenitiesDisplay, setAmenitiesDisplay] = useState();
   const { id } = useParams();
 
+  const getAddress = () => {
+    if (singleProperty) {
+      axios
+        .get(
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${singleProperty.lat},${singleProperty.lng}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`
+        )
+        .then((results) =>
+          api
+            .put(`/listing/${singleProperty.id}`, {
+              formatted_address: results.data.results[0].formatted_address,
+            })
+            .then((response) => console.log(response))
+        );
+    }
+  };
   const getSingleProperty = () => {
     api
       .get(`/listing/${id}`)
       .then((response) => {
-        setSrc(response.data[0].picture_url)
+        setAmenities(response.data[0].amenities);
+        setSrc(response.data[0].picture_url);
         setSingleProperty(response.data[0]);
       })
       .catch((error) => {
         console.error(error);
       });
   };
+  const amenitiesToDisplay = [
+    "Wifi",
+    "Air conditioning",
+    "Cable TV",
+    "Heating",
+    "Kitchen",
+    "Microwave",
+    "Oven",
+    "Refrigerator",
+    "Stove",
+    "Washer",
+  ];
+  const extraImages = {
+    Wifi: wifi,
+    "Air conditioning": airConditioner,
+    "Cable TV": cableTv,
+    Heating: heating,
+    Kitchen: kitchen,
+    Microwave: microwave,
+    Oven: oven,
+    Refrigerator: refrigerator,
+    Stove: stove,
+    Washer: washer,
+  };
+  let extraImageArray = [];
+  const getAmenities = () => {
+    if (amenities) {
+      console.log("object");
+      const amenitiesArray = amenities
+        ? amenities
+            .split(",")
+            .map((amenity) => amenity.replace(/"/g, "").trim())
+        : [];
+
+      extraImageArray = amenitiesToDisplay
+        .map((amenity) => {
+          if (amenitiesArray.includes(amenity)) {
+            return extraImages[amenity];
+          } else {
+            return null;
+          }
+        })
+        .filter((amenity) => amenity !== null);
+      setAmenitiesDisplay(extraImageArray);
+    } else {
+      console.log("THING");
+    }
+  };
+  console.log(extraImageArray);
 
   useEffect(() => {
     getSingleProperty();
   }, [id]);
+
+  useEffect(() => {
+    getAddress();
+  }, []);
+
+  useEffect(() => {
+    getAddress();
+  }, [singleProperty]);
+
+  useEffect(() => {
+    getAmenities();
+  }, [amenities]);
 
   return singleProperty ? (
     <>
@@ -46,19 +138,25 @@ const CardIndividual = () => {
               .replace(/<(\/)?b>/gi, "\n")}
           </p>
           <p className="cardI-view">
-            Address: {singleProperty.formatted_address}
+            <h1 className="cardI-description-title">Address:</h1>
+            {singleProperty.formatted_address
+              ? singleProperty.formatted_address
+              : address}
           </p>
-          <p className="cardI-view">Distances:</p>
           <div className="cardI-facilities">
-            <h1 className="cardI-facilities-title">Facilities</h1>
+            <h1 className="cardI-description-title">Facilities</h1>
             <div className="cardI-facilites-extras">
-              <p className="cardI-facilities-item">OLÁ</p>
-              <p className="cardI-facilities-item">OLÁ</p>
-              <p className="cardI-facilities-item">OLÁ</p>
-              <p className="cardI-facilities-item">OLÁ</p>
-              <p className="cardI-facilities-item">OLÁ</p>
-              <p className="cardI-facilities-item">OLÁ</p>
-              <p className="cardI-facilities-item">OLÁ</p>
+              {amenitiesDisplay && amenitiesDisplay.length > 0
+                ? amenitiesDisplay.map((image, index) => (
+                    <img
+                      className="amenity-icon-individual"
+                      src={image}
+                      style={{ width: "40px", height: "40px" }}
+                      alt={amenitiesToDisplay[index]}
+                      key={index}
+                    />
+                  ))
+                : null}
             </div>
           </div>
         </div>
@@ -66,7 +164,7 @@ const CardIndividual = () => {
           <p className="cardI-price-text">
             From{" "}
             <span className="cardI-price">{singleProperty.price_in_eur}€</span>{" "}
-            / Month
+            / Day
           </p>
           <div className="cardI-button">
             <button type="submit">Enquire</button>
@@ -78,7 +176,7 @@ const CardIndividual = () => {
         </div>
       </div>
 
-      <div>
+      <div className="cardI-google">
         <ChakraProvider theme={theme}>
           <ListingDetailsMap data={singleProperty} />
         </ChakraProvider>
